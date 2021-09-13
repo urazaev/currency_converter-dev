11428d75d06fd5ef2f8ed32d6953cc1cc9e85cb4
alexander <urazaev.alexander@gmail.com>
2021-09-13 13:24:05 +0500

    layout is ready

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // js
+const buttons = document.querySelector('.app__controls');
+
+buttons.addEventListener('click', (evt) => {
+
+    if (evt.target.classList.contains('app__button')) {
+        buttons.querySelectorAll('button').forEach((el) => {
+            el.classList.remove('app__button--active')
+        })
+        evt.target.classList.add('app__button--active')
+    }
+
+})
